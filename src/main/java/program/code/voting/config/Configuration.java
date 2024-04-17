74f9637ad5374e8fcf78c6fcb0f3002630aa29ce
yjailbir <nkudashko@mail.ru>
2024-04-17 14:50:44 +0300

    cors fix2

--- src/main/java/program/code/voting/config/Configuration.java
+++ src/main/java/program/code/voting/config/Configuration.java
@@ -4,12 +4,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @org.springframework.context.annotation.Configuration
-public class Configuration {
-    @Bean
+public class Configuration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+
+    /*@Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowedOrigins(List.of("*"));
@@ -19,5 +30,5 @@ public class Configuration {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);
         return source;
-    }
+    }*/
 }
